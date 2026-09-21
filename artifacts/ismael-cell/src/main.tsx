7bd3acf25b856f086c9e0b8024a44e8f58112547
ismaelmartinses59-hash <ismaelmartinses59@gmail.com>
2026-09-21 19:41:53 -0300

    Limpar cache antigo do app instalado

--- artifacts/ismael-cell/src/main.tsx
+++ artifacts/ismael-cell/src/main.tsx
@@ -2,6 +2,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const CACHE_RESET_VERSION = "caixa-scroll-fix-3";
+if (import.meta.env.PROD && typeof window !== "undefined") {
+  const resetCache = window.localStorage.getItem("ismael-cell-cache-reset");
+  if (resetCache !== CACHE_RESET_VERSION) {
+    window.localStorage.setItem("ismael-cell-cache-reset", CACHE_RESET_VERSION);
+    void Promise.all([
+      navigator.serviceWorker?.getRegistrations().then((registrations) =>
+        Promise.all(registrations.map((registration) => registration.unregister())),
+      ),
+      window.caches?.keys().then((keys) =>
+        Promise.all(keys.map((key) => window.caches.delete(key))),
+      ),
+    ]).then(() => window.location.reload());
+  }
+}
+
 if (import.meta.env.PROD) {
   import("virtual:pwa-register").then(({ registerSW }) => {
     registerSW({
